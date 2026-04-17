b37ba176efa0f12ebc868562f5b5d2bd7253d3a2
SantiagoVillanuevaC <testizigurd@gmail.com>
2026-04-17 17:52:57 -0500

    Update urls.py

--- tienda/urls.py
+++ tienda/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path("productos/nuevo/", views.crear_producto, name="crear_producto"),
     path("productos/<int:pk>/", views.detalle_producto, name="detalle_producto"),
     path("productos/<int:pk>/editar/", views.editar_producto, name="editar_producto"),
+    path("pedidos/<int:pk>/editar/", views.editar_pedido, name="editar_pedido"),
     path("productos/<int:pk>/eliminar/", views.eliminar_producto, name="eliminar_producto"),
 
     # --- Gestión de Clientes ---
